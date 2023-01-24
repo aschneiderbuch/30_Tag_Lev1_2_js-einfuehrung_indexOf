7b8c8e1b211f8b1cf3d8200a5665e1bd13d6f9cc
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-24 12:36:54 +0100

    testingPhase

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,7 @@ console.log(txt.length); // 62 Zeichen
 console.log(txt.indexOf("h")); // 27   inkl. 0 = 28 Zeichen Index Positon 27 = h Index Position 0 = H
 console.log(txt.charAt(27)); // h
 
+console.log(txt.indexOf("Earth")); // 38
+console.log(txt.charAt(38)); // E 
+
+console.log(txt.indexOf("Moon")); // -1     -1 = heißt es kommt nicht vor
