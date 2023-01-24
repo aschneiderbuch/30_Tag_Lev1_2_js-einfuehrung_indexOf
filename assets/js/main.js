08b8ef0f5a1e5d8a99cc3c101d4c00094e997729
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-24 12:34:39 +0100

    Frage

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,12 @@ Suche Position
     Earth
     Moon
 */
+
+
+const txt = "How inappropriate to call this planet Earth, when it is Ocean."
+console.log(txt);
+console.log(txt.length); // 62 Zeichen
+
+console.log(txt.indexOf("h")); // 27   inkl. 0 = 28 Zeichen Index Positon 27 = h Index Position 0 = H
+console.log(txt.charAt(27)); // h
+
